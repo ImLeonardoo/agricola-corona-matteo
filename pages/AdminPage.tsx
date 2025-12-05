cc7703ba8afd9043203ba2548baa7067f36015f6
ImLeonardoo <leonardo080111@gmail.com>
2025-12-05 12:13:50 +0100

    Svuotata la repository

--- pages/AdminPage.tsx
+++
@@ -1,8 +0,0 @@
-// This file is no longer used in the static version of the site.
-import React from 'react';
-
-const AdminPage = () => {
-  return <div>Admin Panel Disabled</div>;
-};
-
-export default AdminPage;
